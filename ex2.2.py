1c387aa427105b72ba027cae87369dd169f98395
Angelo Troncone <98921972+RFblaze@users.noreply.github.com>
2023-02-12 19:42:21 -0700

    Added func1 and func2

--- ex2.2.py
+++ ex2.2.py
@@ -1,5 +1,33 @@
 import json
+import sys
+from time import perf_counter
+import matplotlib.pyplot as plt
+sys.setrecursionlimit(20000)
 
+
+def func1(arr, low, high):
+    if low < high:
+        pi = func2(arr, low, high)
+        func1(arr, low, pi-1)
+        func1(arr, pi + 1, high)
+
+
+def func2(array, start, end):
+    p = array[start]
+    low = start + 1
+    high = end
+    while True:
+        while low <= high and array[high] >= p:
+            high = high - 1
+        while low <= high and array[low] <= p:
+            low = low + 1
+        if low <= high:
+            array[low], array[high] = array[high], array[low]
+        else:
+            break
+    array[start], array[high] = array[high], array[start]
+    return high
+    
 data = json.load(open('ex2.json'))
 count = 0
 counts = []
@@ -14,6 +42,9 @@ for i in range(3):
     count += 1
     timings.append(end - start)
 
-
 plt.plot(counts, timings)
+plt.title("Quick Sort")
+plt.xlabel("Arrays")
+plt.xticks(range(0,3))
+plt.ylabel("Time (s)")
 plt.show()
